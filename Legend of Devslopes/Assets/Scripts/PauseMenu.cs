8c21ecddfdae26f374a42cf4972c7937ce05cebf
Jbakercoe <jbakercoe@gmail.com>
2018-02-03 16:50:50 -0500

    Pause Menu Script
    Added a script to pause the game

---
+++ Legend of Devslopes/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    [SerializeField] GameObject MenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                pause();
+            }
+        }
+	}
+
+    void pause()
+    {
+        MenuUI.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        MenuUI.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        EditorSceneManager.LoadSceneAsync("Menu");
+    }
+
+}
